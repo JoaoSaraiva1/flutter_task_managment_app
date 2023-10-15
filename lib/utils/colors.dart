f83c97c80775feb41747d073ce26a9647c704d37
joaosaraiva <joaosaraiva.s.js@gmail.com>
2023-10-15 11:10:16 +0100

    Criacao da barra superior da aplicacao

---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color tmRed = Color(0xFFDA4040);
+const Color tmBlue = Color(0xFF5F52EE);
+
+const Color tmBlack = Color(0xFF3A3A3A);
+const Color tmGrey = Color(0xFF717171);
+
+const Color tmBGColor = Color.fromARGB(205, 43, 140, 38);
